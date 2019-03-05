5433b854256a0e3262018b61bb6b22cf017df75c
Jefwillems <jefwillems100@gmail.com>
2019-03-05 16:00:22 +0100

    add postback data in channeldata

--- src/dialogs/QuestionDialog.ts
+++ src/dialogs/QuestionDialog.ts
@@ -109,6 +109,7 @@ export default class QuestionDialog extends WaterfallDialog {
                 title: 'Download',
                 type: ActionTypes.ImBack,
                 value: { content: document.resourceURI },
+                channelData: { postback: { content: document.resourceURI } },
               },
             ],
           );
--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,6 @@ adapter.onTurnError = async (context, error) => {
   console.error(`\n [onTurnError]: ${error}`);
   console.log(error.stack);
   console.log(error.stack);
-  console.log(JSON.stringify(error['request']));
   console.log(JSON.stringify(error['response']));
 
   // Send a message to the user.
